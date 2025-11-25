9a4a86fb381f0425a75882341350b8e872e13b29
Ignacio del Valle Alles <nacho@earthly.dev>
2025-11-25 22:38:40 +0100

    Enhance main function for JSON response support
    Updated the main function to support JSON responses based on a feature flag.

--- main.go
+++ main.go
@@ -1,30 +1,46 @@
 package main
-
+ 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
-	"github.com/sirupsen/logrus"
-)
+	"github.com/sirupsen/logrus" 
+)    
 
+// feature flag to always return JSON
+var alwaysJSON = strings.EqualFold(os.Getenv("FF_ALWAYS_JSON"), "true")
+ 
 func main() {
-
-	quotes, err := Quotes()
+	quotes, err := Quotes()  
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		// common headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Max-Age", "15")
+
 		quote := RandomQuote(quotes)
-
 		logrus.Info(quote)
+
+		if alwaysJSON {
+			// JSON response
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+			// simple JSON shape: { "quote": "<text>" }
+			_ = json.NewEncoder(w).Encode(map[string]string{
+				"quote": quote,
+			})
+			return
+		}
+
+		// plain text response
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		fmt.Fprint(w, quote)
-
-		// fmt.Fprint(w, "Hello from Earthly - 12:28PM")
-
 	})
 
 	http.ListenAndServe(":80", nil)
